3919cc29baff86703cd013416877fbeb0e3fb585
Sam Smith <phuedx@wikimedia.org>
2025-05-15 13:23:34 +0100

    ext.xLab: Send events to a different event intake service URL
    Additional changes:
    
    * Update user-facing, inline documentation, emphasizing that
      Experiment#constructor() is package-private and calling it outside
      xLab is unsupported
    
    Bug: T391988
    Depends-On: I3d78ec8749cceb851d21930facfd3ef7376dab9b
    Change-Id: I539895e18b1bd4a34d50ecd4feecb3674f9dbc4c

--- tests/qunit/ext.xLab/Experiment.test.js
+++ tests/qunit/ext.xLab/Experiment.test.js
@@ -1,18 +1,107 @@
 const { Experiment } = mw.xLab;
 
-QUnit.module( 'ext.xLab/Experiment', () => {
-	const e = new Experiment( 'hello_world', 'A' );
+QUnit.module( 'ext.xLab/Experiment', QUnit.newMwEnvironment( {
+	beforeEach: function () {
+		this.metricsClient = {
+			submitInteraction: this.sandbox.spy()
+		};
 
-	QUnit.test.each(
-		'isAssignedGroup()',
-		{
-			A: [ 'A', true ],
-			B: [ 'B', false ],
-			'Multiple, including A': [ [ 'B', 'A' ], true ],
-			'Multiple, excluding A': [ [ 'B', 'C' ], false ]
-		},
-		( assert, [ groups, expected ] ) => {
-			assert.strictEqual( e.isAssignedGroup( ...groups ), expected );
+		// Note well that Experiment#constructor() is package-private. Calling it outside xLab is
+		// not supported.
+
+		this.everyoneExperiment = new Experiment(
+			this.metricsClient,
+			'hello_world',
+			'A',
+			'awaiting',
+			'edge-unique',
+			'xLab'
+		);
+
+		this.loggedInExperiment = new Experiment(
+			this.metricsClient,
+			'my-awesome-experiment',
+			'B',
+			'0x0ff1ce',
+			'mw-user',
+			'xLab'
+		);
+	}
+} ) );
+
+QUnit.test.each(
+	'isAssignedGroup()',
+	{
+		A: [ 'A', true ],
+		B: [ 'B', false ],
+		'Multiple, including A': [ [ 'B', 'A' ], true ],
+		'Multiple, excluding A': [ [ 'B', 'C' ], false ]
+	},
+	function ( assert, [ groups, expected ] ) {
+		assert.strictEqual( this.everyoneExperiment.isAssignedGroup( ...groups ), expected );
+	}
+);
+
+QUnit.test.each(
+	'send() - sends events via metricsClient',
+	[
+		[
+			'everyoneExperiment',
+			{
+				enrolled: 'hello_world',
+				assigned: 'A',
+				subject_id: 'awaiting',
+				sampling_unit: 'edge-unique',
+				coordinator: 'xLab'
+			}
+		],
+		[
+			'loggedInExperiment',
+			{
+				enrolled: 'my-awesome-experiment',
+				assigned: 'B',
+				subject_id: '0x0ff1ce',
+				sampling_unit: 'mw-user',
+				coordinator: 'xLab'
+			}
+		]
+	],
+	function ( assert, [ propertyName, expectedExperiment ] ) {
+		this[ propertyName ].send( 'Hello, World!' );
+
+		assert.strictEqual( this.metricsClient.submitInteraction.called, true );
+		assert.deepEqual( this.metricsClient.submitInteraction.firstCall.args, [
+			'product_metrics.web_base',
+			'/analytics/product_metrics/web/base/1.4.2',
+			'Hello, World!',
+			{
+				experiment: expectedExperiment
+			}
+		] );
+	}
+);
+
+QUnit.test( 'send() - overrides experiment field', function ( assert ) {
+	this.everyoneExperiment.send( 'Hello, World!', {
+		experiment: {
+			foo: 'bar',
+			baz: 'qux'
 		}
-	);
+	} );
+
+	assert.strictEqual( this.metricsClient.submitInteraction.called, true );
+	assert.deepEqual( this.metricsClient.submitInteraction.firstCall.args, [
+		'product_metrics.web_base',
+		'/analytics/product_metrics/web/base/1.4.2',
+		'Hello, World!',
+		{
+			experiment: {
+				enrolled: 'hello_world',
+				assigned: 'A',
+				subject_id: 'awaiting',
+				sampling_unit: 'edge-unique',
+				coordinator: 'xLab'
+			}
+		}
+	] );
 } );
